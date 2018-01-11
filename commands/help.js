2eedbd5eed60bf634f10d8d73626a2f84ee8f3dc
Tyler <34672818+ClientMod@users.noreply.github.com>
2018-01-11 22:37:30 +0000

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -15,7 +15,7 @@ exports.run = (RolesCrazy, message, args) => {
         const embed = new Discord.RichEmbed()
         .setColor(hexcols[~~(Math.random() * hexcols.length)])
         .addField(`General Commands`, `${PREFIX}create <Role Name> - Creates a role with the provided name\n${PREFIX}setcolor <Role Name> <Color (eg: 0x00ffff or #00ffff)> - Set the provided names color to the provided color\n${PREFIX}help - displays this message`)
-        .addField(`Moderation Commands`, `${PREFIX}setprefix <Prefix> - sets the prefix to the provided prefix`)
+        .addField(`Moderation Commands`, `${PREFIX}setprefix <Prefix> - sets the prefix to the provided prefix\n${PREFIX}purge <Messages (1 - 99)> - Deletes the specified amount of messages`)
         .addField(`Owner Only`, `*reload <Command> - Reloads specified command`)
         message.channel.send({embed});
     })
